8871a307d5010bd288b278c8fb3edc32c707d468
ksw117 <ksw11768@gmail.com>
2026-01-20 11:16:45 +0900

    .

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.gnb > ul > li > a').hover(function() {
+        $('header').stop().animate({'background-color' : '#000'}, 200);
+    }, function() {
+        $('header').stop().animate({'background-color' : 'rgba(255, 255, 255, 0.1)'}, 200);
+    });
+
+});
