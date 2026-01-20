f28e60cb2fe788ea17f7319a4c862f7e5590c4f4
ksw117 <ksw11768@gmail.com>
2026-01-20 10:19:10 +0900

    Add files via upload

---
+++ hybe/js/carousel.js
@@ -0,0 +1,24 @@
+document.addEventListener(`DOMContentLoaded`, function () {
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".custom-next",
+            prevEl: ".custom-prev",
+        },
+
+
+
+    });
+
+
+});
